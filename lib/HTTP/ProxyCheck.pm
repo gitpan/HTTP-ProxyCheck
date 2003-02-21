d034e40dc6822b4f07a304e93164f433697ff9bb
Thomas Weibel <thom@cpan.org>
2003-02-21 16:39:35 +0000

    Import of THOM/HTTP-ProxyCheck-1.0 from CPAN.
    gitpan-cpan-distribution: HTTP-ProxyCheck
    gitpan-cpan-version:      1.0
    gitpan-cpan-path:         THOM/HTTP-ProxyCheck-1.0.tar.gz
    gitpan-cpan-author:       THOM
    gitpan-cpan-maturity:     released

--- lib/HTTP/ProxyCheck.pm
+++ lib/HTTP/ProxyCheck.pm
@@ -16,7 +16,7 @@ use IO::Socket;
 use vars qw($VERSION $answer $errstr $reason);
 
 BEGIN {
-	$VERSION = 0.2;
+	$VERSION = 1.0;
 	$answer = '';
 	$errstr = '';
 	$reason = '';
@@ -152,8 +152,8 @@ sub _check_proxy {
 
   	$tmp = <<"REQUEST";
 GET $args{url} HTTP/1.0
-Referer: http://www.perl.org/
-User-Agent: ProxyCheck/0.1
+Referer: None
+User-Agent: HTTP::ProxyCheck/0.1
 Host: $args{dochost}
 Pragma: no-cache
 Accept: image/gif, image/x-xbitmap, image/jpeg, image/pjpeg, image/png, */*
@@ -228,27 +228,48 @@ HTTP::ProxyCheck - Checks HTTP proxy servers.
 
 =head1 SYNOPSIS
 
+=head2 CHECK HTTP PROXY
+
   use HTTP::ProxyCheck;
 
   # $OUTPUT_AUTOFLUSH
   $| = 1;
 
+  my $proxycheck = new HTTP::ProxyCheck();
   my $proxy = "10.0.0.1:8080";
   my $url = "http://www.perl.org/index.shtml";
 
   print "Trying to connect to $proxy and retrieve $url\n";
 
-  if (HTTP::ProxyCheck->check(proxy=>"$proxy", url => "$url")) {
-	  print "$proxy returns: " . HTTP::ProxyCheck->get_answer . "\n";
+  if ($proxycheck->check(proxy=> "$proxy",url=> "$url",answer=>"header")) {
+  	print "$proxy returns: " . $proxycheck->get_answer() . "\n";
   }
   else {
-	  print "Error (" . HTTP::ProxyCheck->get_reason . ")\n";
+  	print "Error (" . $proxycheck->get_reason() . ")\n";
+  }
+
+=head2 CHECK PROXY ADDRESS
+
+  use HTTP::ProxyCheck;
+
+  my $proxycheck = new HTTP::ProxyCheck();
+  my $proxy = "10.0.0.1:8080";
+
+  print "Checking proxy address $proxy\n";
+
+  if ($proxycheck->check_address($proxy) {
+  	print "$proxy is a valid proxy address\n";
+  }
+  else {
+  	print "Error (" . $proxycheck->get_reason() . ")\n";
   }
 
 =head1 DESCRIPTION
 
 HTTP::ProxyCheck is a class to check HTTP proxy servers. It connects to given 
-HTTP proxy servers and tries to retrieve a provided URL through them.
+HTTP proxy servers and tries to retrieve a provided URL through them. You 
+can also use HTTP::ProxyCheck to check wether the syntax of a proxy 
+address may be valid without connecting to a server.
 
 The return message from the proxy servers can be accessed through the 
 C<get_answer> method.
@@ -258,25 +279,26 @@ method.
 
 =head1 METHODS
 
-=head2 check(proxy => "$proxy", url => "$url" [, answer => "header" | "full"])
+=head2 check( proxy => $proxy, url => $url, answer => $type )
 
 The C<check> method is used to check a HTTP proxy server. The C<check> method
-includes a test to check the syntax of the provided proxy server address and
-URL. You can specify wether you want to get only the header or the full page 
-as return value from the method. This specification is optionally. The default 
-answer is "header".
+includes a test to check the syntax of the provided proxy server address 
+(C<check_address>) and URL. You can specify wether you want to get only the 
+header ($type = "header") or the full page ($type = "full") as return value 
+from the method. This specification is optionally. The default answer is 
+$type = "header".
 
-=head2 check_address($proxy)
+=head2 check_address( $proxy )
 
 The C<check_address> method is used to check for a valid proxy server address. When 
 you use C<check> the proxy address is checked with this method automatically.
 
-=head2 get_answer()
+=head2 get_answer( )
 
 Through the C<get_answer> method the return message from the proxy servers can be 
-accessed
+accessed.
 
-=head2 get_reason()
+=head2 get_reason( )
 
 When a proxy check fails, the reason can be accessed through the C<get_reason> 
 method.
